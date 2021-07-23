14e6c590608cab92baa19312766c13a060c52470
Naqhid <mnaqhid@gmail.com>
2021-07-23 19:32:00 +0530

    solve-issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import logo from './bookstore-logo.png';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="!#" className="App-link" target="_blank" rel="noopener noreferrer">
-          React Bookstore
-        </a>
-      </header>
+      <h1>Hey there</h1>
     </div>
   );
 }
