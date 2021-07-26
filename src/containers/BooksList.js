ccaaf8278f22a0fa99a5f09467df913953a71da8
Naqhid <mnaqhid@gmail.com>
2021-07-26 14:43:43 +0530

    issues-solve

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,38 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { actionFilter, actionRemove } from '../actions';
+import { actionRemove } from '../actions';
 import store from '../store';
-import CategoryFilter from '../components/CategoryFilter';
 
-const BooksList = ({ books, filter }) => {
+const BooksList = ({ books }) => {
   const handleBookRemove = (id) => {
     store.dispatch(actionRemove({ id }));
   };
 
-  const handleFilterChange = (e) => {
-    store.dispatch(actionFilter(e.target.value));
-  };
-
-  let filteredBooks = books;
-
-  if (filter !== 'ALL') {
-    filteredBooks = books.filter((book) => book.category === filter);
-  }
-
   return (
     <div>
-
-      <CategoryFilter handleFilterChange={handleFilterChange} />
-
       <table>
         <tr>
           <th>Book ID</th>
           <th>Title</th>
           <th>Category</th>
         </tr>
-        {
-        filteredBooks.map((book) => (
+        {books.map((book) => (
           <Book
             id={book.id}
             title={book.title}
@@ -41,8 +26,7 @@ const BooksList = ({ books, filter }) => {
             key={book.id}
             removeBook={handleBookRemove}
           />
-        ))
-}
+        ))}
       </table>
     </div>
   );
@@ -50,12 +34,8 @@ const BooksList = ({ books, filter }) => {
 
 BooksList.propTypes = {
   books: PropTypes.instanceOf(Array).isRequired,
-  filter: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  books: state.appReducer.books,
-  filter: state.filterReducer.category,
-});
+const mapStateToProps = (state) => ({ books: state.appReducer.books });
 
 export default connect(mapStateToProps)(BooksList);
